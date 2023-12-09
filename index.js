774c205457a0ae73327ee1bbef541c422b429f78
Carlston Rebelo <130921730+Carlston325@users.noreply.github.com>
2023-12-09 16:14:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const webSize = window.matchMedia("(max-width: 760px)");
+let video = document.querySelector("video");
+let videoSrc = ["./assets/videos/CO89119_Yearly_Reunion_Social_Adapts_620_x_1126_V2.mp4", "./assets/videos/CO89119_Yearly_Reunion_Social_Adapts_1600x640__2_.mp4"];
 
-if (webSize.matches) {
-    document.querySelector(".heroVideo").setAttribute("src", "./assets/videos/CO89119_Yearly_Reunion_Social_Adapts_620_x_1126_V2.mp4");
+function getScreenSize() {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth <= 767) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
+
+function loadVideoSource() {
+    let screenSize = getScreenSize();
+
+    video.pause();
+    video.src = videoSrc[screenSize];
+    video.play();
+}
+
+loadVideoSource();
+window.addEventListener("resize", loadVideoSource);
