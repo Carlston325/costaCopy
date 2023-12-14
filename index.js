11b81c0c22d67a2a73156582641ef889c2248e8d
Carlston Rebelo <130921730+Carlston325@users.noreply.github.com>
2023-12-14 11:17:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let video = document.querySelector("video");
+let video = document.getElementById("video");
 let videoSrc = ["./assets/videos/CO89119_Yearly_Reunion_Social_Adapts_620_x_1126_V2.mp4", "./assets/videos/CO89119_Yearly_Reunion_Social_Adapts_1600x640__2_.mp4"];
 
 function getScreenSize() {
@@ -14,21 +14,10 @@ function getScreenSize() {
 function loadVideoSource() {
     let screenSize = getScreenSize();
 
-    video.pause;
+    video.pause();
     video.src = videoSrc[screenSize];
-    video.play;
+    video.play();
 }
 
 loadVideoSource();
 window.addEventListener("resize", loadVideoSource);
-
-window.addEventListener("resize", function(){
-    let screenSize = window.innerWidth;
-
-    if (screenSize <= 1000) {
-        document.querySelector("body > div > div:nth-child(1) > div.container.col-xxl-8.px-4.py-5 > div.row.flex-lg-row-reverse.align-items-center.g-5.my-4.py-5 > div.col-lg-6.text-center").classList.add("text-center");
-    } else {
-        document.querySelector("body > div > div:nth-child(1) > div.container.col-xxl-8.px-4.py-5 > div.row.flex-lg-row-reverse.align-items-center.g-5.my-4.py-5 > div.col-lg-6.text-center").classList.remove("text-center");
-    }
-});
-
