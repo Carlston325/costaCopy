e65d94b98aabc8844edec4bc110f79fbd55fd16d
Carlston Rebelo <130921730+Carlston325@users.noreply.github.com>
2023-12-18 17:37:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// HERO VIDEO
 let video = document.getElementById("video");
 let videoSrc = ["./assets/videos/CO89119_Yearly_Reunion_Social_Adapts_620_x_1126_V2.mp4", "./assets/videos/CO89119_Yearly_Reunion_Social_Adapts_1600x640__2_.mp4"];
+let header = ["headerSmall", "headerLarge"]
 
 function getScreenSize() {
     let screenWidth = window.innerWidth;
@@ -22,10 +24,58 @@ function loadVideoSource() {
 loadVideoSource();
 window.addEventListener("resize", loadVideoSource);
 
+// HEADER
 const menu = $("#headerSmall");
 const closeButton = $(".closeMenuBTN");
 
-$(closeButton).on("click", function() {
-    menu.addClass("menuHide");
-    console.log(menu.addClass("menuHide"))
-})
+function toggleMenuIn() {
+    menu.addClass("menuHide70");
+    setTimeout(function(){
+        menu.removeClass("menuHide70");
+        menu.addClass("menuHide50");
+    }, 20);
+
+    setTimeout(function(){
+        menu.removeClass("menuHide50");
+        menu.addClass("menuHide30");
+    }, 40);
+    
+    setTimeout(function(){
+        menu.addClass("menuHideFULL");
+    }, 80);
+}
+
+$(closeButton).on("click", toggleMenuIn);
+
+function toggleMenuOut() {
+    menu.addClass("menuHide30");
+    setTimeout(function(){
+        menu.removeClass("menuHide30");
+        menu.addClass("menuHide50");
+    }, 30);
+
+    setTimeout(function(){
+        menu.removeClass("menuHide50");
+        menu.addClass("menuHide70");
+    }, 50);
+    
+    setTimeout(function(){
+        menu.removeClass("menuHide70");
+        menu.addClass("menuHideNone");
+    }, 70);
+}
+
+function headerScreen () {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth <= 1050) {
+        toggleMenuOut();
+        $("#headerLarge").slideUp();
+    } else {
+        $("#headerLarge").slideDown();
+        toggleMenuIn();
+    }
+}
+
+headerScreen();
+window.addEventListener("resize", headerScreen);
